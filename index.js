deeaf42b2865775197c978c6cc3b2d0bcf97cc44
Subham Kumar <subhamkr5925@gmail.com>
2025-01-16 00:22:19 +0530

    Almost complete

--- index.js
+++ index.js
@@ -1,34 +1,92 @@
+const guess = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessButton");
+const wordDisplay = document.getElementById("wordDisplay");
+const guessed = document.getElementById("guesses");
 
-
-const words = ['javascript', 'programming', 'hangman', 'developer', 'openai', 'react' , 'node', 'express'];
+const words = [
+  "javascript",
+  "programming",
+  "hangman",
+  "developer",
+  "openai",
+  "react",
+  "node",
+  "express",
+];
 let selectedWord = words[Math.floor(Math.random() * words.length)];
 let guessedLetters = [];
 let remainingAttempts = 6;
+let displayed = "";
+console.log(selectedWord);
 
+window.onload = function () {
+  setWord();
+};
 
+guessButton.addEventListener("click", changeWord);
 
+function setWord() {
+  for (let i = 0; i < selectedWord.length; i++) {
+    displayed += "_";
+  }
+
+  wordDisplay.textContent = displayed;
+}
+
+function changeWord() {
+  let code = guess.value.charCodeAt(0);
+  if (!(code > 64 && code < 91) && !(code > 96 && code < 123)) {
+    return;
+  }
+  if (!guessedLetters.includes(guess.value)) {
+    guessedLetters.push(guess.value);
+    guessed.innerHTML = guessedLetters.join(", ");
+
+    if (selectedWord.includes(guess.value)) {
+      displayed = displayed.split("");
+
+      let foundIndex = Array.from(
+        selectedWord.matchAll(`${guess.value}`),
+        (match) => match.index
+      );
+
+      foundIndex.forEach((element) => {
+        displayed[element] = `${guess.value}`;
+      });
+
+      displayed = displayed.join("");
+      wordDisplay.textContent = displayed;
+      checkWin();
+    }
+
+    remainingAttempts--;
+    checkLoss();
+    guess.value = "";
+  } else {
+    alert("Guess a new Letter");
+  }
+}
 
 function setWinBackgroundColor() {
-  document.body.style.backgroundColor = 'green';
+  document.body.style.backgroundColor = "green";
 }
 
 function setLossBackgroundColor() {
-  document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = "red";
 }
 
-
 function checkWin() {
- 
-    wordDisplay.textContent = 'Congratulations! You won!';
-
-
-  
+  if (selectedWord == displayed) {
+    wordDisplay.style.letterSpacing = `1px`;
+    wordDisplay.textContent = "Congratulations! You won!";
+    setWinBackgroundColor();
+  }
 }
 
 function checkLoss() {
-
-    wordDisplay.textContent = 'Game over! You lost.';
- 
-  
+  if (remainingAttempts < 0) {
+    wordDisplay.style.letterSpacing = `1px`;
+    wordDisplay.textContent = "Game over! You lost.";
+    setLossBackgroundColor();
+  }
 }
-
